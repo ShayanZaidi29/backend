d4d6ee60d885fffae20e02b315b43353914feb37
Shayan <biffgarfield21@gmail.com>
2023-09-25 12:59:40 +0530

    Backend created

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const connectDB = require('./db'); // Check the path to db.js
+
+async function startServer() {
+    try {
+        await connectDB(); // Call the connectDB function to establish the database connection
+
+        // Rest of your code
+        // ...
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!');
+        });
+
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`);
+        });
+    } catch (error) {
+        console.error('Error starting server:', error);
+    }
+}
+
+startServer();
