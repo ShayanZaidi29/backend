71f9f5089e17a4b78aac4884fc31bd309944bb22
Shayan <biffgarfield21@gmail.com>
2023-09-25 16:19:45 +0530

    Password validation added

--- Routes/CreateUser.js
+++ Routes/CreateUser.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
+router.post("/createuser",[ 
+body('email','Incorrect email').isEmail(),
+body('name').isLength({min: 5}),
+body('password','Incorrect Password').isLength({min: 5})]
+,async (req,res) =>{
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-router.post("/createuser", async (req,res) =>{
+
+
     try{
         await User.create({
             name: req.body.name,
